cd43d8a3013a955522d06d9935db62678d2a313d
Audra K <audrak@Audras-MacBook-Pro.local>
2018-01-24 15:04:33 -0800

    modified gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,22 @@ var gulp = require('gulp'), //Load gulp first!//
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
     browserSync = require('browser-sync').create();
+    eslint = require('gulp-eslint');
 
-gulp.task('script', function(){
+gulp.task('lint', function() {
+  return gulp.src(['./js/*.js'])
+  .pipe(eslint())
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError())
+});    
+
+
+gulp.task('script', gulp.series('lint', function () {
   return gulp.src('./js/*.js')
   .pipe(uglify())
   .pipe(rename({ extname: '.min.js' }))
   .pipe(gulp.dest('./build/js'))
-});
+}));
 
 gulp.task('watch', function() {
   gulp.watch('js/*.js', gulp.series('script'));
